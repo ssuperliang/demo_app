390bc6cde3456aa8840d1e9c4c78077501bb81df
ssuperliang <seubmelc@163.com>
2017-06-11 16:04:13 +0800

    Serve static assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  #使程序在生产环境中能服务静态文件
+  config.serve_static_assets = true
 end
