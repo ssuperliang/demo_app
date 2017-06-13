a5ebe72fa015efedf21408200a83d823d4667292
ssuperliang <seubmelc@163.com>
2017-06-13 22:22:03 +0800

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
   # match '/', to: 'static_pages#home', via: 'get'
   root to: 'static_pages#home'
@@ -7,6 +8,10 @@ Rails.application.routes.draw do
   match '/help',to: 'static_pages#help',via: 'get'
   match '/about',to: 'static_pages#about',via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
+
+  match '/signin', to: 'sessions#new', via: 'get'
+  match '/signin', to: 'sessions#new', via: 'get'
+  match '/signout', to: 'sessions#destroy', via: 'delete'
 
 
   # The priority is based upon order of creation: first created -> highest priority.
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170612135934) do
+ActiveRecord::Schema.define(version: 20170613125416) do
 
   create_table "microposts", force: :cascade do |t|
     t.string   "content"
@@ -26,8 +26,10 @@ ActiveRecord::Schema.define(version: 20170612135934) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "remember_token"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token"
 
 end
